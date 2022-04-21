1e504c25ccd24add8118b476325ee286cfe916ec
Rishabh Verma <rishabhvrm43@gmail.com>
2022-04-21 02:27:42 +0000

    Thu, Apr 21, 2022, 2:27 AM +00:00

--- calculator.py
+++ calculator.py
@@ -1,9 +1,7 @@
-x = input("What's x? ")
-y = input("What's y? ")
+x = int(input("What's x? "))
+y = int(input("What's y? "))
 
-z = int(x) + int(y)
-
-print(z)
+print(x + y)
 
 a = input("Another input please: ")
 b = int(a)
