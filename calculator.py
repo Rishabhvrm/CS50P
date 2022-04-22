83749d1d1757092b0d9bb75395d28b4114433114
Rishabh Verma <rishabhvrm43@gmail.com>
2022-04-22 02:02:49 +0000

    Fri, Apr 22, 2022, 2:02 AM +00:00

--- calculator.py
+++ calculator.py
@@ -1,8 +1,12 @@
-x = int(input("What's x? "))
-y = int(input("What's y? "))
+x = float(input("What's x? "))
+y = float(input("What's y? "))
 
-print(x + y)
+print(round(x + y))
 
+
+'''
+checking if int() would take a string
 a = input("Another input please: ")
 b = int(a)
 print(b)
+'''
