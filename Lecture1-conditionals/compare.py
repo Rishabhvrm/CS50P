5e079ab55bc63148a6146a0d804fb9b967033916
Rishabh Verma <rishabhvrm43@gmail.com>
2022-04-26 05:28:43 +0000

    Tue, Apr 26, 2022, 5:28 AM +00:00

--- Lecture1-conditionals/compare.py
+++ Lecture1-conditionals/compare.py
@@ -1,7 +1,17 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
+
+# if, elif, else
 if x > y:
     print("x is greater than y")
 elif x < y:
     print("x is less than y")
+else:
+    print("x is equal to y")
+
+# or
+if x > y or x < y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
