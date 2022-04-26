e2d1fd7901acb5875c9cdfd55389e2203d77a5cb
Rishabh Verma <rishabhvrm43@gmail.com>
2022-04-26 05:45:23 +0000

    Tue, Apr 26, 2022, 5:45 AM +00:00

---
+++ Lecture1-conditionals/grade.py
@@ -0,0 +1,9 @@
+score = int(input("What's your Grade? "))
+
+# comparison in a range
+if 90 <= score < 100:
+    print("Grade A")
+elif 80 <= score < 90:
+    print("Grade B")
+else:
+    print("C or below")
