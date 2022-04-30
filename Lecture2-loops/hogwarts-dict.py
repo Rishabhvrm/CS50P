941656f4315bfb92afa73d0b6529e97fa664e0da
Rishabh Verma <rishabhvrm43@gmail.com>
2022-04-30 18:46:38 +0000

    Sat, Apr 30, 2022, 6:46 PM +00:00

---
+++ Lecture2-loops/hogwarts-dict.py
@@ -0,0 +1,9 @@
+# dictionary, allows us to associate something with something else
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco":"Slytherin"
+}
+
+print (students["Hermione"])
