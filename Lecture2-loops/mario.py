7583aa4740c43eb4c0ec37df6ac69b245e41e8e5
Rishabh Verma <rishabhvrm43@gmail.com>
2022-04-30 19:59:05 +0000

    Sat, Apr 30, 2022, 7:59 PM +00:00

--- Lecture2-loops/mario.py
+++ Lecture2-loops/mario.py
@@ -1,8 +1,21 @@
 def main():
     print_square(3)
+    print_square2(3)
+
 
 def print_square(size):
     for _ in range(size):
         print("#" * 3)
+
+def print_square2(size):
+    # for each row in square
+    for i in range(size):
+
+        # for each brick in a row
+        for j in range(size):
+
+            # Print brick
+            print("#", end = "")
+        print()
 
 main()
