896579eaaeab61f5f53940ef523c51081b02d641
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-05 06:23:54 +0000

    Thu, May 5, 2022, 6:23 AM +00:00

--- Lecture3-exceptions/hello.py
+++ Lecture3-exceptions/hello.py
@@ -2,10 +2,11 @@
 
 # try and except and else
 
-while true:
+while True:
     try:
         x = int(input("What's x? "))
     except ValueError:
         print("Please provide an integer only")
     else:
         print(f"x is {x}")
+        break
