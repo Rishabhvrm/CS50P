e8989e14938ea44552e716e77cb5352cf8795cea
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-09 04:01:55 +0000

    Mon, May 9, 2022, 4:01 AM +00:00

--- Lecture4-modules-libraries/itunes.py
+++ Lecture4-modules-libraries/itunes.py
@@ -8,4 +8,8 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+
+print(response"results[trackName]")
+
 print(json.dumps(response.json(), indent=2))
+
