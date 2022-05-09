820a3b49af21a96d3166cc59a603b8d8b5b1abda
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-09 03:10:41 +0000

    Mon, May 9, 2022, 3:10 AM +00:00

--- Lecture4-modules-libraries/name.py
+++ Lecture4-modules-libraries/name.py
@@ -10,8 +10,11 @@ except IndexError:
 # Check for errors
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
-elif len(sys.argv) > 2:
+#elif len(sys.argv) > 2:
     sys.exit("Too many arguments")
 
+for arg in sys.argv:
+    print(arg)
+
 # Print name tags
-print("hello, my name is", sys.argv[1])
+#print("hello, my name is", sys.argv[1])
