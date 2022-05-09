d3887c8eb395c612430b2cc04494e813bb4e9cc7
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-09 03:50:44 +0000

    Mon, May 9, 2022, 3:50 AM +00:00

---
+++ Lecture4-modules-libraries/itunes.py
@@ -0,0 +1,10 @@
+# Demonstrates Requests
+
+import sys
+import requests
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/search?entity=songs&limit=1&term=weezer" + sys.argv[1])
+print(response.json())
