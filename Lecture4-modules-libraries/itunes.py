7ced0ccb4eebd30575c0ebde383bb123c09dbc53
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-09 03:56:35 +0000

    Mon, May 9, 2022, 3:56 AM +00:00

--- Lecture4-modules-libraries/itunes.py
+++ Lecture4-modules-libraries/itunes.py
@@ -1,5 +1,6 @@
 # Demonstrates Requests
 
+import json
 import sys
 import requests
 
@@ -7,4 +8,4 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=songs&limit=1&term=" + sys.argv[1])
-print(response.json())
+print(json.dumps(response.json()))
