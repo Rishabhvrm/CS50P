58d6274db254eb7839daeb411443edd309a24bf1
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-09 02:39:29 +0000

    Mon, May 9, 2022, 2:39 AM +00:00

--- Lecture4-modules-libraries/generate.py
+++ Lecture4-modules-libraries/generate.py
@@ -1,8 +1,16 @@
 import random
 # from random import choice
 
+# using choice(seq)
 coin = random.choice(["heads", "tails"])
 print(coin)
 
+# using randint(a,b)
 randomInteger = random.randint(1,10)
 print(randomInteger)
+
+# using shuffle(x)
+# it shuffles the argument in place
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+print(cards)
