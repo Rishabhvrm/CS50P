c6d90d39a44848184fe9bafe4cb15a3da4526774
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-17 06:10:51 +0000

    Tue, May 17, 2022, 6:10 AM +00:00

--- Lecture5-unit-tests/test_calculator.py
+++ Lecture5-unit-tests/test_calculator.py
@@ -14,8 +14,14 @@ def test_square():
 
 # using assert
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 squared is not 4, test case failed")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 squared is not 9, test case failed")
 
 if __name__ == "__main__":
     main()
