787ae4f511c34414530ab08b9ddf540d141b1d49
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-17 06:30:35 +0000

    Tue, May 17, 2022, 6:30 AM +00:00

--- Lecture5-unit-tests/test2_calculator.py
+++ Lecture5-unit-tests/test2_calculator.py
@@ -2,9 +2,13 @@
 
 from calculator import square
 
-def test_square():
+def test_square_positive():
     assert square(2) == 4
     assert square(3) == 9
+
+def test_square_negative():
     assert square(-2) == 4
     assert square(-3) == 9
+
+def test_square_zero():
     assert square(0) == 0
