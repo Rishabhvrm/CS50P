0040e4f4b01eb3af3de1fa904d978bf3b69d6f34
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-17 06:07:54 +0000

    Tue, May 17, 2022, 6:07 AM +00:00

--- Lecture5-unit-tests/test_calculator.py
+++ Lecture5-unit-tests/test_calculator.py
@@ -3,11 +3,19 @@ from calculator import square
 def main():
     test_square()
 
+# without assert
+'''
 def test_square():
     if square(2) != 4:
         print("2 squared was not 4, test case failed")
     if square(3) != 9:
         print("3 squared was not 9, test case failed")
+'''
+
+# using assert
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
 
 if __name__ == "__main__":
     main()
