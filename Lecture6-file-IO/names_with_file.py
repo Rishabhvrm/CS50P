0bbf42ca72e6c1b3476a566a4c4ad3f7ec50e42e
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-23 12:13:33 +0000

    Mon, May 23, 2022, 12:13 PM +00:00

--- Lecture6-file-IO/names_with_file.py
+++ Lecture6-file-IO/names_with_file.py
@@ -1,5 +1,9 @@
+# open and with
+
 name = input("Please enter your name? ")
 
-file = open("names.txt", "a")
-file.write(f"{name}\n")
+#file = open("names.txt", "a")
+
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
 file.close()
