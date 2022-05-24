e457d00e373acd8b963b4c248630e4296aa5df90
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-24 07:33:31 +0000

    Tue, May 24, 2022, 7:33 AM +00:00

--- Lecture6-file-IO/students_with_file.py
+++ Lecture6-file-IO/students_with_file.py
@@ -5,3 +5,19 @@ with open("students_hogwarts.csv") as file:
         # assigning 2 variables at once
         name, house = line.rstrip().split(",")
         print(f"{name} is in {house}")
+
+print("HOLD UP!!!")
+
+# if I want to sort it
+students = []
+
+with open("students_hogwarts.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
