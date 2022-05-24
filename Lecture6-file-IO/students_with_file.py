401bc233ae04148c2477f3a5cc80db28c72a0a28
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-24 07:24:43 +0000

    Tue, May 24, 2022, 7:24 AM +00:00

--- Lecture6-file-IO/students_with_file.py
+++ Lecture6-file-IO/students_with_file.py
@@ -1,4 +1,7 @@
 with open("students_hogwarts.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        #row = line.rstrip().split(",")
+        #print(f"{row[0]} is in {row[1]}")
+        # assigning 2 variables at once
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
