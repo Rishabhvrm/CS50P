2a87c274f515b2d69fab17df1e4d678c59564645
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-25 08:19:38 +0000

    Wed, May 25, 2022, 8:19 AM +00:00

--- Lecture6-file-IO/PIL/costumes.py
+++ Lecture6-file-IO/PIL/costumes.py
@@ -7,3 +7,8 @@ images = []
 # take the cmd line arguments, but skip the first (program name)
 for arg in sys.argv[1:]:
     image = Image.open(arg)
+    images.append(image)
+
+images[0].save(
+    "costumes.gif", save_all=True, appned_images=[images[1]], duration=200, loop=0
+)
