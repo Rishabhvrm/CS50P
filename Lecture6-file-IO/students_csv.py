a63bcba4cb58fbf33cf2708586eee8c5e277c1d2
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-25 07:36:22 +0000

    Wed, May 25, 2022, 7:36 AM +00:00

--- Lecture6-file-IO/students_csv.py
+++ Lecture6-file-IO/students_csv.py
@@ -3,12 +3,16 @@ import csv
 students = []
 
 with open("students_home.csv") as file:
-    #reader = csv.reader(file), stores values as a list of colum
-    # using DictReader, not a list of columns but a dictionary of columns
-    reader = csv.DictReader(file)
-
+    # stores values as a list of colum
+    '''
+    reader = csv.reader(file)
     for name, home in reader:
         students.append({"name": name, "home": home})
+    '''
+    # using DictReader, not a list of columns but a dictionary of columns
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name"}: row["name"], {"home"}: row["home"])
 
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is from {student['home']}")
