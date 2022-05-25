ec57d506bcfe35d4d32dd885a3353dc771175adb
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-25 07:31:24 +0000

    Wed, May 25, 2022, 7:31 AM +00:00

--- Lecture6-file-IO/students_csv.py
+++ Lecture6-file-IO/students_csv.py
@@ -3,7 +3,9 @@ import csv
 students = []
 
 with open("students_home.csv") as file:
-    reader = csv.reader(file)
+    #reader = csv.reader(file), stores values as a list of colum
+    # using DictReader, not a list of columns but a dictionary of columns
+
     for name, home in reader:
         students.append({"name": name, "home": home})
 
