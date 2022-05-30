ff5f04fabbcc51724506d6b90f4d3ce43a2c2884
Rishabh Verma <rishabhvrm43@gmail.com>
2022-05-30 12:34:43 +0000

    Mon, May 30, 2022, 12:34 PM +00:00

--- Lecture7-regex/validate.py
+++ Lecture7-regex/validate.py
@@ -1,8 +1,18 @@
 email = input("What's your email? ").strip()
 
-
+# eg 1
 # 'pythonic' way of doing checking a character in a string
 if "@" in email:
+    print("Valid")
+else:
+    print("Invalid")
+
+# eg 2
+email2 = input("Okay, give me another email: ").strip()
+
+username, domain = email2.split("@")
+
+if (username) and ("." in domain):
     print("Valid")
 else:
     print("Invalid")
