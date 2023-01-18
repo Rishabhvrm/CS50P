c89b5afc6a4a63df12d9e0797994d6a24c27f47f
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-18 03:00:06 +0000

    Wed, Jan 18, 2023, 3:00 AM +00:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,3 +1,8 @@
+# get user input
 user_input = input()
+
+# split user input by space into a list
 word_list = user_input.split(' ')
+
+# join the elements in the list with '...' (a period)
 print('...'.join(word_list))
