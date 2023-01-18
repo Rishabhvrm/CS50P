963baccb944777f9568d8b3ceddba64006f96c24
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-18 03:18:53 +0000

    Wed, Jan 18, 2023, 3:18 AM +00:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,8 +1,14 @@
+# define main function
 def main():
+    # get user input
     user_input = input()
+
+    # call convert function and print the output
     print(convert(user_input))
 
+# define convert function
 def convert(s):
+    # replace the smiley faces with emojis
     return s.replace(":)","🙂").replace(":(","🙁")
 
 main()
