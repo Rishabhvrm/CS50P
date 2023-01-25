7da1c8c8d9b210108a409ed02589576a1cf93539
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-25 12:32:46 +0000

    Wed, Jan 25, 2023, 7:32 AM -05:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,4 +1,7 @@
-x = input()
-values = x.split()
+# take user input
+user_input = input().strip().split()
+
+# initialize x, y and z
 x, y, z = int(values[0]), values[1], int(values[2])
+
 print(x,y,z)
