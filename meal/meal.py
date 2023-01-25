aa6c1802c62d406f6c733362e34c3b128fd6bc80
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-25 14:49:49 +0000

    Wed, Jan 25, 2023, 9:49 AM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,11 +1,19 @@
 def main():
     time = input("What time is it? ").strip()
-    convert(time)
+    print(convert(time))
 
 def convert(time):
     hours, minutes = map(int, time.split(":"))
-    minutes/60
+    t =  hours + minutes/60
 
+    if 7 <= t <= 8:
+        meal_time = "breakfast time"
+    elif 12 <= t <= 13:
+        meal_time = "lunch time"
+    elif 18 <= t <= 19:
+        meal_time = "dinner time"
+
+    return meal_time
 
     # map the float time to what meal it is
 
