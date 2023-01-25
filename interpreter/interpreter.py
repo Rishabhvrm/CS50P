09df10281713d1a512d4dcc2d1bf81787b59a34a
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-25 12:41:13 +0000

    Wed, Jan 25, 2023, 7:41 AM -05:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -4,6 +4,8 @@ user_input = input().strip().split()
 # initialize x, y and z
 x, y, z = int(user_input[0]), user_input[1], int(user_input[2])
 
+ans = 0.0
+
 match y:
     case '+':
         ans = x + y
@@ -12,8 +14,7 @@ match y:
     case '*':
         ans = x * y
     case '/':
-        ans = x / y
+        if z != 0:
+            ans = x / y     # if y == /, then z can't be 0
 
-# if y == /, then z can't be 0
-ans = xyz
 print(ans)
