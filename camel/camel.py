5a6c3ad98336c5ebaac13c29a7f9b5c6fcdb6917
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-25 18:14:24 +0000

    Wed, Jan 25, 2023, 1:14 PM -05:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,6 +1,13 @@
 user_input = input("camelCase: " )
 
 # split the words ?
+for c in user_input:
+    if c.isupper():
+        print("_" + c.lower(), end = "")
+    else:
+        print(c, end = "")
+print()
+
 
 # lower the words
 
