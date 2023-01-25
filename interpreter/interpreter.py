584dbb0180788882f2a5a719f9d6ebcfcca0a9e7
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-25 12:45:38 +0000

    Wed, Jan 25, 2023, 7:45 AM -05:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,5 +1,5 @@
 # take user input
-user_input = input().strip().split()
+user_input = input("Expression: ").strip().split()
 
 # initialize x, y and z
 x, y, z = int(user_input[0]), user_input[1], int(user_input[2])
