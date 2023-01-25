15047b68f827c5acddcd429676f94eb604bfa301
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-25 18:19:11 +0000

    Wed, Jan 25, 2023, 1:19 PM -05:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,14 +1,15 @@
+# take user input
 user_input = input("camelCase: " )
 
-# split the words ?
+# check if a character is upper or lower
+# if lower then print as is
+# if upper then convert to lower and prepend with underscore
+# set 'end' = "" in print thereby overriding the default '/n' new-line
 for c in user_input:
     if c.isupper():
         print("_" + c.lower(), end = "")
     else:
         print(c, end = "")
+
+# move the cursor to a new line
 print()
-
-
-# lower the words
-
-# add an underscore btw the words and output
