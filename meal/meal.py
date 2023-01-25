5842a9bf0b4a0129d929da3ff9866db88c65ab8b
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-25 15:33:35 +0000

    Wed, Jan 25, 2023, 10:33 AM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -16,7 +16,8 @@ def convert(time):
     PM = "p.m."
 
     # if time is in (24 hour format)
-    if AM and PM not in time:
+    if (AM and PM) not in time:
+        print(time)
         # split the time into hours and minutes
         hours, minutes = map(int, time.split(":"))
 
