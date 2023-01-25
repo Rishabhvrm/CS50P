742a15a5ed5ce2ce8ecde41496ed4060c7b25912
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-25 16:59:30 +0000

    Wed, Jan 25, 2023, 11:59 AM -05:00

--- 2023/Lecture-2-Loops/user-input-cat.py
+++ 2023/Lecture-2-Loops/user-input-cat.py
@@ -3,6 +3,8 @@
 # keep asking the user to provide n until it's a positive number
 # i.e. keep repeating the question if user provides a negative number
 
+# A VERY COMMON PARADIGM IN PYTHON
+
 while True:
     n = int(input("What's n? "))
     # break out as soon as you get a positive number
