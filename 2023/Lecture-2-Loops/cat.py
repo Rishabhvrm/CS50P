319b165cb5f49527ed06431f4543f8bebb35955c
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-25 16:43:45 +0000

    Wed, Jan 25, 2023, 11:43 AM -05:00

---
+++ 2023/Lecture-2-Loops/cat.py
@@ -0,0 +1,12 @@
+# making this file
+#  > to demonstrate the use of '_' (underscore) in a loop
+#  > to show '*' operator in print statement
+
+for _ in range(3):
+    print("meow")
+
+# when required to go through a loop and we don't use the variable
+# it's a convention to use _ (underscore) as that variable instead of naming it 'i' or somethin else
+
+# same as above
+print("\nmeow" * 3)
