55c02e158bf27cc86d4a09c99d6a68a27c5e0820
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-26 14:56:45 +0000

    Thu, Jan 26, 2023, 9:56 AM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,10 +1,10 @@
 def main():
     while True:
         cost = 50
-        print(f"Amount Due: " {cost})
+        print(f"Amount Due: {cost}")
         coin_denom = int(input("Insert Coin: "))
         if coin_denom in [25, 10, 5]:
-            cost = calculate_due_amount(cost - coin_denom)
+            cost = calculate_due_amount(cost, coin_denom)
             break
 
     print("yay")
