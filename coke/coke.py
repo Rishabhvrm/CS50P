001e2c7253d3679cf10b1eda180887f1bd9a9996
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-26 15:27:11 +0000

    Thu, Jan 26, 2023, 10:27 AM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,23 +3,19 @@ def main():
     # initialize amount due before enterinig any coins
     amount_due = 50
 
-    # prompt the user repeatedly
-    # until coin_denom is in [25, 10, 5]
+    # prompt the user repeatedly until coin_denom is in [25, 10, 5]
     while True:
-
         print(f"Amount Due: {amount_due}")
         coin_denom = int(input("Insert Coin: "))
 
-        # continue if user enters correct coin denomination
+        # continue only if user enters correct coin denomination
         if coin_denom in [25, 10, 5]:
 
-            # calculate amount due
+            # update amount_due
             amount_due = calculate_due_amount(amount_due, coin_denom)
 
-            # break if amount due goes 0 or negative
-            # i.e. user has paid full amount or extra
+            # break if amount due goes 0 or negative i.e. user has paid full amount or extra
             if amount_due <= 0:
-
                 # print change owed and change the sign to make it +ve
                 print(f"Change Owed: {-amount_due}")
                 break
