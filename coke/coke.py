2d9c19b1b7bf1df9a7225efdb8633b0395edb624
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-26 15:13:06 +0000

    Thu, Jan 26, 2023, 10:13 AM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,9 +4,9 @@ def main():
         print(f"Amount Due: {amount_due}")
         coin_denom = int(input("Insert Coin: "))
 
-        amount_due = calculate_due_amount(amount_due, coin_denom)
 
         if coin_denom not in [25, 10, 5] and amount_due > 0:
+            amount_due = calculate_due_amount(amount_due, coin_denom)
             continue
 
         if amount_due <= 0:
