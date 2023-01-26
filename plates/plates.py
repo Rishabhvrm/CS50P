da5d66fa39ad1916ed87ebc9ed869333f9f1852a
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-26 17:40:39 +0000

    Thu, Jan 26, 2023, 12:40 PM -05:00

---
+++ plates/plates.py
@@ -0,0 +1,23 @@
+def main():
+    plate = input("Plate: ")
+    print("Valid" if is_valid(plate) else "Invalid")
+
+def is_valid(s):
+    valid = False
+
+    starts_with_two_letters(s)
+    max_min_length(s)
+    num_at_end(s)
+        first_num_zero(s)
+    no_period(s)
+
+    if (a and b and c and d):
+        valid = True
+
+    return valid
+
+def starts_with_two_letters(s):
+    for c in s:
+        
+
+main()
