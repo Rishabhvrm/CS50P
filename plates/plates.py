96cbe8e51d2da02405bd63f2685f22c0585057b2
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-26 17:52:48 +0000

    Thu, Jan 26, 2023, 12:52 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -2,16 +2,16 @@ import string
 
 def main():
     plate = input("Plate: ")
-    print("Valid" if is_valid(plate) else "Invalid")
+    print("Valid" if is_valid(plate) else ".")
 
 def is_valid(s):
     valid = False
 
-    starts_with_two_letters(s)
-    max_min_length(s)
+    #starts_with_two_letters(s)
+    #max_min_length(s)
     #num_at_end(s)
     #first_num_zero(s)
-    #no_period(s)
+    no_period(s)
 
     #if (a and b and c and d):
     #    valid = True
@@ -20,6 +20,14 @@ def is_valid(s):
 
 def starts_with_two_letters(s):
         print (s[0] in string.ascii_letters and s[1] in string.ascii_letters)
+
+# https://docs.python.org/3/library/string.html
+def no_period(s):
+    print(string.punctuation)
+    print(string.punctuation not in s)
+    print(string.whitespace)
+    print(string.whitespace not in s)
+    print (string.punctuation not in s and string.whitespace not in s)
 
 main()
 
