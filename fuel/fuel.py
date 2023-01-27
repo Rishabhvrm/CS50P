a2969e6b904ef6c10c55e42f53207a4fd607a1f5
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-27 19:27:44 +0000

    Fri, Jan 27, 2023, 2:27 PM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -3,14 +3,20 @@ def main():
         try:
             x, y = input("Fraction: ").strip().split('/')
             x, y = int(x), int(y)
+
+            # convert in percentage
             p =  round(100 * x/y)
+
+            # prompt again if x > y
             if (x > y):
                 continue
+        # if user enters non-integer value 
         except (ValueError, ZeroDivisionError):
             pass
 
         else:
             # check 'Full' or 'Empty' conditions
+            # add % sign if p is between (1,99)
             print(f"{p}%" if (1 < p < 99) else ('E' if p <= 1 else 'F'))
             break
 
