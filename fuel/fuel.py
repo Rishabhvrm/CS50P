bc326238c1869cfcbe96b4a263afd77d5b3d4696
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-27 18:19:20 +0000

    Fri, Jan 27, 2023, 1:19 PM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,14 +1,16 @@
 def main():
-    try:
-        x, y = input("Fraction: ").strip().split('/')
-        x, y = int(x), int(y)
-    except ValueError:
-        pass
-    else:
-        p = calculate_percent(x,y)
+    while True:
+        try:
+            x, y = input("Fraction: ").strip().split('/')
+            x, y = int(x), int(y)
+        except ValueError or :
+            pass
+        else:
+            p = calculate_percent(x,y)
 
-        # check 'Full' or 'Empty' conditions
-        print(f"{p}%" if (1 <= p <= 99) else ('E' if p < 1 else 'F'))
+            # check 'Full' or 'Empty' conditions
+            print(f"{p}%" if (1 <= p <= 99) else ('E' if p < 1 else 'F'))
+            break
 
 
 def calculate_percent(x,y):
