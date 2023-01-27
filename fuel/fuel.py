0a8b842f4a439e2d367cb2896a7a22e659c04eba
Rishabh Verma <rishabhvrm43@gmail.com>
2023-01-27 18:27:00 +0000

    Fri, Jan 27, 2023, 1:27 PM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -4,8 +4,9 @@ def main():
             x, y = input("Fraction: ").strip().split('/')
             x, y = int(x), int(y)
             p =  round(100 * x/y)
-        except (ValueError, ZeroDivisionError):
+        except (ValueError, ZeroDivisionError)
             pass
+        
         else:
             # check 'Full' or 'Empty' conditions
             print(f"{p}%" if (1 <= p <= 99) else ('E' if p < 1 else 'F'))
