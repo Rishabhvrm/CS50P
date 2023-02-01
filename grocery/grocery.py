8086e6a350b2101ace61548fe8303c09d8f27c19
Rishabh Verma <rishabhvrm43@gmail.com>
2023-02-01 02:34:17 +0000

    Tue, Jan 31, 2023, 9:34 PM -05:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,8 +1,9 @@
-from collections import Counter
+from collections import Counter, OrderedDict
 
-item_dict = dict()
+# initialise an empty list
+item_list = []
 
-
+# keep prompting the user until CTRL + D
 while True:
 
     try:
@@ -10,6 +11,15 @@ while True:
     except EOFError:
         break
     else:
-        item_list.add(item)
+        # append to the list until CTRL + D
+        item_list.append(item)
 
-print(Counter(item_list))
+# use 'Counter' to count the occurance of each itme in the list
+# convert it into a dict, store both value and occurance (we need both the values, hence dict)
+# sort the dict using keys on dict.items, sorted returns a list
+# convert it into a dict again
+d = dict(sorted(dict(Counter(item_list)).items()))
+
+# print value and key
+for k,v in d.items():
+    print(f"{v} {k.upper()}")
