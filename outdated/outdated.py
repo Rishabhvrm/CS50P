d93cffc956bad0aa9fcf5e4310a90faecc9604cf
Rishabh Verma <rishabhvrm43@gmail.com>
2023-02-14 21:52:26 +0000

    Tue, Feb 14, 2023, 4:52 PM -05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -24,12 +24,34 @@ is_date = False
 if date[0].isdigit():
     if 1<= date[0] <= 12:
         is_date = True
+        num_date = True
+
+
 elif date[0].isalpha():
-    if
+    a, b = date.split(' ', maxsplit=1)
+    if a.title() in months:
+        is_date = True
+        word_date = True
+
+# if date is in correct format
+# break out of the loop
+if is_date:
+    # break
+    pass
+
 
 print(date[0])
-    # if date is in correct format
-        # break out of the loop
+print(a)
+print(b)
+
 
 # format the date
+# if date is in format 9/8/1636
+if num_date:
+    month, date, year = date.split('/').strip()
+    print(f"{year}-{month:02}-{date:02}")
+
+if word_date:
+
+
 # print out put
