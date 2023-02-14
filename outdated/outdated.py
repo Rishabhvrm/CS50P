30817caea076d43fcd97536c11000b97f8af7caf
Rishabh Verma <rishabhvrm43@gmail.com>
2023-02-14 22:21:23 +0000

    Tue, Feb 14, 2023, 5:21 PM -05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -13,35 +13,33 @@ months = {
     "December": 12
 }
 
-#while True:
-
-date = input("Date: ").strip()
 is_date = False
 word_date, num_date = False, False
+
+while True:
+    date = input("Date: ").strip()
+
 
 # determine if date is in
 # this format September 8, 1636
 # or this 9/8/1636
-if date[0].isdigit():
-    if 1<= int(date[0]) <= 12:
-        is_date = True
-        num_date = True
+    if date[0].isdigit():
+        if 1<= int(date[0]) <= 12:
+            is_date = True
+            num_date = True
 
-elif date[0].isalpha():
-    print("it's word date guys")
-    a, b = date.split(' ', maxsplit=1)
-    if a.title() in months.keys():
-        is_date = True
-        word_date = True
+    elif date[0].isalpha():
+        a, b = date.split(' ', maxsplit=1)
+        if a.title() in months.keys():
+            is_date = True
+            word_date = True
 
-# if date is in correct format
-# break out of the loop
-if is_date:
-    # break
-    pass
+    # if date is in correct format
+    # break out of the loop
+    if is_date:
+        break
+        #pass
 
-
-print(date[0])
 #print(a)
 #print(b)
 
@@ -57,4 +55,4 @@ if word_date:
     print(f"{year}-{months[month]:02}-{date[:-1].zfill(2)}")
 
 
-# print out put
+# print output
