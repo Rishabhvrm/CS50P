6c3e5db4cf8115823de6154fbdb1d13ead997d82
Rishabh Verma <rishabhvrm43@gmail.com>
2023-02-14 21:42:03 +0000

    Tue, Feb 14, 2023, 4:42 PM -05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -16,12 +16,15 @@ months = [
 #while True:
 
 date = input("Date: ")
+is_date = False
 
 # determine if date is in
 # this format September 8, 1636
 # or this 9/8/1636
 if date[0].isdigit():
-    if date[0] < 
+    if 1<= date[0] <= 12:
+        is_date = True
+elif date[0].
 
 print(date[0])
     # if date is in correct format
