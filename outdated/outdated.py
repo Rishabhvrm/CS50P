b004d6e6b9821465b58cc43b22bf44b1abd078a1
Rishabh Verma <rishabhvrm43@gmail.com>
2023-02-14 21:59:02 +0000

    Tue, Feb 14, 2023, 4:59 PM -05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,19 +15,20 @@ months = [
 
 #while True:
 
-date = input("Date: ")
+date = input("Date: ").strip()
 is_date = False
+word_date, num_date = False, False
 
 # determine if date is in
 # this format September 8, 1636
 # or this 9/8/1636
 if date[0].isdigit():
-    if 1<= date[0] <= 12:
+    if 1<= int(date[0]) <= 12:
         is_date = True
         num_date = True
 
-
 elif date[0].isalpha():
+    print("it's word date guys")
     a, b = date.split(' ', maxsplit=1)
     if a.title() in months:
         is_date = True
@@ -41,17 +42,19 @@ if is_date:
 
 
 print(date[0])
-print(a)
-print(b)
+#print(a)
+#print(b)
 
 
 # format the date
 # if date is in format 9/8/1636
 if num_date:
-    month, date, year = date.split('/').strip()
+    month, date, year = date.split('/')
     print(f"{year}-{month:02}-{date:02}")
 
 if word_date:
+    month, date, year = date.split(' ').strip()
+    print(f"{year}-{month:02}-{date:02}")
 
 
 # print out put
