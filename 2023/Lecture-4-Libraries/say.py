cc6373a72d362841ad868fb68560e03311ccd0dc
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-09 23:46:08 +0000

    Fri, Jun 9, 2023, 7:46 PM -04:00

--- 2023/Lecture-4-Libraries/say.py
+++ 2023/Lecture-4-Libraries/say.py
@@ -9,3 +9,9 @@ import sys
 if len(sys.argv) == 2:
     cowsay.cow("hello, " + sys.argv[1])
     cowsay.trex("hello, " + sys.argv[1])
+
+
+
+# ---------------------------------------------------------------
+
+# APIs - third party services that we can write code to talk to
