5bfbd06a739a449831dd424feb11a961b2e184b7
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-09 23:19:33 +0000

    Fri, Jun 9, 2023, 7:19 PM -04:00

--- 2023/Lecture-4-Libraries/generate.py
+++ 2023/Lecture-4-Libraries/generate.py
@@ -29,4 +29,7 @@ print(statistics.mean([100,90]))
 # sys.argv
 import sys
 
-print('hello, my name is', sys.argv[1])
+try:
+    print('hello, my name is', sys.argv[1])
+except IndexError:
+    print('Too few arguments')
