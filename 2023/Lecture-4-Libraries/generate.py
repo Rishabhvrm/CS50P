badbeeeac5edb640d9f16547b7951b9fa284b3f1
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-09 23:07:05 +0000

    Fri, Jun 9, 2023, 7:07 PM -04:00

--- 2023/Lecture-4-Libraries/generate.py
+++ 2023/Lecture-4-Libraries/generate.py
@@ -1,4 +1,19 @@
 import random
+# coin = random.choice(["heads", "tails"])
 
-coin = random.choice(["heads", "tails"])
+# use directly 'import' or use 'from' and then function name
+
+from random import choice
+
+# choose a random value from the given sequence
+coin = choice(["heads", "tails"])
 print(coin)
+
+# choose a random number inclusive of the parameters
+number = random.randint(1,10)
+print(number)
+
+# shuffle the argument 'in-place'
+cards = ['jack', 'queen', 'king']
+random.shuffle(cards)
+print(cards)
