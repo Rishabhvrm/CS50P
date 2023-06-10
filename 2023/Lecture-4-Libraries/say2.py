b4dad9f48706fbe24f4a75b69710c656d55fb55e
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-10 00:10:03 +0000

    Fri, Jun 9, 2023, 8:10 PM -04:00

---
+++ 2023/Lecture-4-Libraries/say2.py
@@ -0,0 +1,3 @@
+import sys
+
+# calling my own library/
---
+++ 2023/Lecture-4-Libraries/sayings.py
@@ -0,0 +1,12 @@
+def main():
+    hello('world')
+    goodbye('people')
+
+def hello(name):
+    print(f'hello, {name}')
+
+def goodbye(name):
+    print(f'goodbye, {name}')
+
+
+main()
