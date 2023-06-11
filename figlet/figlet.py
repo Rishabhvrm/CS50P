7db3b2d9c97176ae53193795fdb76e6d7aa1076f
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-11 21:39:57 +0000

    Sun, Jun 11, 2023, 5:39 PM -04:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,8 +8,18 @@ fonts = figlet.getFonts()
 
 if len(sys.argv) == 1 or len(sys.argv) == 3:
     print('y')
+    if sys.argv[1] not in ['-f','--font'] or sys.argv[2] not in fonts:
+        sys.exit('Check the command line inputs')
 
+    # set font based on given font or randomly
+    if len(sys.argv) == 3:
+        font = sys.argv[2]
+    else:
+        font = random.choice(fonts)
 
+    figlet.setFont(font)            # set font
+    str = input()                   # input user string
+    print(figlet.renderText(str))   # print output
 
 
 # if second_arg isEmpty():
