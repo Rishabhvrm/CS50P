c788df769b61748af579719986dc2ce3580dd401
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-11 21:02:45 +0000

    Sun, Jun 11, 2023, 5:02 PM -04:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -2,7 +2,3 @@ import emoji
 
 input = input('Input: ')
 print('Output:', emoji.emojize(input))
-
-# print(emoji.emojize('python is :thumbs_up:'))
-# print(emoji.emojize('python is :red_heart:'))
-# print(emoji.emojize("Python is :red_heart:", variant="emoji_type"))
