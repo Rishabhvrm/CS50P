66c75d9231096b746fdf6982e44e3e8fcd9ba5cb
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-11 20:52:05 +0000

    Sun, Jun 11, 2023, 4:52 PM -04:00

--- 2023/Lecture-4-Libraries/sayings.py
+++ 2023/Lecture-4-Libraries/sayings.py
@@ -17,4 +17,7 @@ if __name__ == "__main__":
     main()
 
 # __name__ 's  default value is 'main' when you run a file from command line
-# hence it will not be run when called when it is being imported in another file becuase its defualt value won't be 'main'
+# hence it will not be run when it is being imported in another file becuase its defualt value won't be 'main'
+
+# Because it is not being run from the command line, instead it is being called from another file. Hence, the value
+# for '__name__' will not be 'main'
