f6de3b3e869b5acbdc3774d697572eea60356147
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-12 23:29:01 +0000

    Mon, Jun 12, 2023, 7:29 PM -04:00

--- game/game.py
+++ game/game.py
@@ -1,13 +1,13 @@
 import random
 
-n = input('Level: ')
+n = int(input('Level: '))
 
 # choose a random number inclusive of the parameters
 number = random.randint(1,n)
 print(number)
 
 
-guess = input('Guess: ')
+guess = int(input('Guess: '))
 
 output = 'just right' if guess == number else 'large' if guess > number else 'small'
 
