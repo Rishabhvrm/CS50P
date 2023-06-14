05f9b0cb9301315e3607200d0e02609933540b31
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-14 22:01:46 +0000

    Wed, Jun 14, 2023, 6:01 PM -04:00

---
+++ professor/professor.py
@@ -0,0 +1,16 @@
+import random
+
+def main():
+    level = get_level()
+
+def get_level():
+    try:
+        level = input("Level: ")
+        
+
+
+def generate_integer():
+    pass
+
+def __name__ = "__main__":
+    main()
