bf92647e349ff5f990af314693ad8adb09bebd16
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-14 23:58:54 +0000

    Wed, Jun 14, 2023, 7:58 PM -04:00

--- professor/professor.py
+++ professor/professor.py
@@ -4,7 +4,7 @@ def main():
     level = get_level()
     print(level)
     int = generate_integer(level)
-    
+
 
 # get levels. 1, 2, 3 only
 # prompt again if user enters anything other than 1, 2, 3
@@ -21,7 +21,11 @@ def get_level():
                 return level
 
 
-def generate_integer():
+def generate_integer(level):
+    if level == 1:
+        a, b = random.randint(0,9), randint(0, 9)
+    elif level == 2:
+        a, b = random.randint(10, 99), random.randint(10, 99)
     pass
 
 if __name__ == "__main__":
