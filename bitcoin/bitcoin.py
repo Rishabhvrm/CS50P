6b77f75ab6ea5a4fbde51a0353461ae06deb2ecc
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-15 19:52:51 +0000

    Thu, Jun 15, 2023, 3:52 PM -04:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,6 +1,9 @@
 import requests
+import sys
 
 # takes 1 command line argument as a float, sys.exit() if argument not float, missing or not a number
 # Call api and check the price, handle exceptions
 # outputs cost of n bitcoins in USD using thousand ,(comma separator) upto 4 digit decimal
 
+if len(sys.argv < 2)
+sys.argv[1] ! =
