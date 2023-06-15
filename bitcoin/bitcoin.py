726759c5ea7877b0e278cd37993df9e15129f119
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-15 20:02:59 +0000

    Thu, Jun 15, 2023, 4:02 PM -04:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -8,6 +8,10 @@ import sys
 if len(sys.argv) < 2:
     sys.exit('Missing command-line argument')
 
-if type(sys.argv[1]) != float:
+try:
+    n = float(sys.argv[1])
+except:
     sys.exit('Command-line argument is not a number')
 
+
+print(n)
