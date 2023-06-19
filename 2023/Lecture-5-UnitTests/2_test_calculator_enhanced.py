bbec51d2c168e3ccc4e081594d1702b5e83162e9
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-19 18:05:27 +0000

    Mon, Jun 19, 2023, 2:05 PM -04:00

--- 2023/Lecture-5-UnitTests/2_test_calculator_enhanced.py
+++ 2023/Lecture-5-UnitTests/2_test_calculator_enhanced.py
@@ -1,5 +1,6 @@
 # introducing pytest
 # unit test - testing individual units of your program
+# pytest will run all the functions
 
 from calculator import square
 
@@ -8,4 +9,15 @@ def test_square():
     assert square(3) == 9
     assert square(-2) == 4
     assert square(-3) == 9
+    assert square(0) == 0
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+def test_zero():
     assert square(0) == 0
