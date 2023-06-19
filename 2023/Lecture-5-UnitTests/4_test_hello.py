76aa61ef12c16e530cc70d8c57d95783df20988d
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-19 18:30:33 +0000

    Mon, Jun 19, 2023, 2:30 PM -04:00

---
+++ 2023/Lecture-5-UnitTests/4_test_hello.py
@@ -0,0 +1,12 @@
+def main():
+    test_default()
+    test_argument()
+
+def test_default():
+    assert() == "hello, world"
+
+def test_argument():
+    assert("david") == "hello, david"
+
+if __name__ == "__main__":
+    main()
