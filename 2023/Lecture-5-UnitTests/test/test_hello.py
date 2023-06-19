d5c2525245f653818eb147f711948175c38e4cea
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-19 18:40:45 +0000

    Mon, Jun 19, 2023, 2:40 PM -04:00

--- 2023/Lecture-5-UnitTests/test/test_hello.py
+++ 2023/Lecture-5-UnitTests/test/test_hello.py
@@ -1,4 +1,10 @@
 from c_hello import hello
 
-a = hello('rish')
-print(a)
+def test_default():
+    assert hello() == "hello, world"
+
+def test_argument():
+    assert hello("david") == "hello, david"
+
+# Running tests on a package/folder
+# have to create a file '__init__.py' so that python can treat this folder/module as a package
