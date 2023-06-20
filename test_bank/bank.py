049071a24a63b27f5a966a31386966a815c38370
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-20 14:35:55 +0000

    Tue, Jun 20, 2023, 10:35 AM -04:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -3,11 +3,9 @@ def main():
     print(f'${value(greeting.lower())}')
 
 def value(greeting):
-    if greeting == "hello":
-        return 100
-    elif greeting[0] == "h":
-        return 20
-    else: return 0
+    if greeting == "hello": return 0
+    elif greeting[0] == "h": return 20
+    else: return 100
 
 if __name__ == "__main__":
     main()
