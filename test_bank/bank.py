a2c3570428156498492705524d6fb8c1443b5ed8
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-20 14:06:31 +0000

    Tue, Jun 20, 2023, 10:06 AM -04:00

---
+++ test_bank/bank.py
@@ -0,0 +1,13 @@
+def main():
+    greeting = input("Greeting: ")
+    print(f'${value(greeting)}')
+
+def value(greeting):
+    if greeting == "hello":
+        return 100
+    elif greeting[0] == "h":
+        return 20
+    else: return 0
+
+if __name__ == "__main__":
+    main()
