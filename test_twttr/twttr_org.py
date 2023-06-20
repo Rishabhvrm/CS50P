e3810bc3206821232d7bce16dbfaeda6107cdbf2
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-20 01:56:06 +0000

    Mon, Jun 19, 2023, 9:56 PM -04:00

---
+++ test_twttr/twttr_org.py
@@ -0,0 +1,12 @@
+user_input = input("Input: ")
+
+# create a list of vowels
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+out_str = []
+
+for c in user_input:
+    if c not in vowels:
+        out_str.append(c)
+
+print(f"Output: {''.join(out_str)}")
