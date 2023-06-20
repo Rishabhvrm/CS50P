1d2d899d03f1b0ddbb11d0865d4652842a0c6a29
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-20 02:11:06 +0000

    Mon, Jun 19, 2023, 10:11 PM -04:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -3,8 +3,8 @@ def main():
     print("Output:", shorten(word))
 
 def shorten(word):
-    # vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    vowels = ['A', 'E', 'I', 'O', 'U']
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    # vowels = ['A', 'E', 'I', 'O', 'U']
     output = []
     for _ in word:
         if _ not in vowels:
@@ -13,15 +13,3 @@ def shorten(word):
 
 if __name__ == "__main__":
     main()
-# user_input = input("Input: ")
-
-# # create a list of vowels
-# vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-
-# out_str = []
-
-# for c in user_input:
-#     if c not in vowels:
-#         out_str.append(c)
-
-# print(f"Output: {''.join(out_str)}")
