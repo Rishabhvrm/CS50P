6829d543754c09eb7662e8f2c2a67037078f75a9
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-20 14:45:03 +0000

    Tue, Jun 20, 2023, 10:45 AM -04:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,4 +1,22 @@
 from bank import value
 
-test_uppercase():
-    assert value('HELLO') == '
+def test_uppercase():
+    assert value('HELLO') == 0
+
+def test_lowercase():
+    assert value("hello") == 0
+
+def test_sentence():
+    assert value("hello, how are you?") == 20
+
+def test_sentence_wrong():
+    assert value('what?') == 100
+
+def test_numbers():
+    assert value(123) == 100
+
+def test_whitespace():
+    assert value(" ") == 100
+
+def test_punctuation():
+    assert value(",.") == 100
