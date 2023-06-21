4b5eb5163353fe34a8b1aa4828f77b565d57069d
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-21 21:12:41 +0000

    Wed, Jun 21, 2023, 5:12 PM -04:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -13,5 +13,14 @@ def test_convert_return_full():
 def test_gauge_return():
     assert gauge(50) == "50%"
 
-def test_gauge_return():
-    assert gauge()
+def test_gauge_return_empty1():
+    assert gauge(1) == "E"
+
+def test_gauge_return_empty1():
+    assert gauge(0) == "E"
+
+def test_gauge_return_full1():
+    assert gauge(99) == "F"
+
+def test_gauge_return_full2():
+    assert gauge(100) == "F"
