b5bf3302aba6e0608b70aac26be5f0f4f0fa0865
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-21 20:28:25 +0000

    Wed, Jun 21, 2023, 4:28 PM -04:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,17 @@
+def main():
+    try:
+        x, y = input("Fraction: ").strip().split('/')
+        x, y = int(x), int(y)
+        convert(x/y)
+
+
+def convert(fraction):
+    # convert in percentage
+    p =  round(100 * x/y)
+
+
+def gauge(percentage):
+
+
+if __name__ == "__main__":
+    main()
