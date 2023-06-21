3206609dd99ee97b5115e59284195448ebacdfa8
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-21 21:34:38 +0000

    Wed, Jun 21, 2023, 5:34 PM -04:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,5 +1,6 @@
 from fuel import convert
 from fuel import gauge
+import pytest
 
 def test_convert_return_correct():
     assert convert("1/2") == 50
@@ -25,8 +26,23 @@ def test_gauge_return_empty2():
 def test_gauge_return_full1():
     assert gauge(99) == "F"
 
-def test_convert_value_error():
-    assertRaises convert("cat/dog") raise ValueError
-
 def test_gauge_return_full2():
     assert gauge(100) == "F"
+
+# making sure that a ValueError will be raised
+# when a string is passed instead of an integer
+def test_convert_invalid_input():
+    with pytest.raises(ValueError):
+        convert("cat/dog")
+
+# making sure that a ValueError will be raised
+# when x > y
+def test_convert_invalid_input2():
+    with pytest.raises(ValueError):
+        convert("2/1")
+
+# making sure that a ZeroDivisonError will be raised
+# when y = 0 is passed
+def test_convert_invalid_y():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
