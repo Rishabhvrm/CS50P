da21e84737d74981469753e18679d3184441f730
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-21 21:03:50 +0000

    Wed, Jun 21, 2023, 5:03 PM -04:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -4,8 +4,11 @@ from fuel import gauge
 def test_convert_return_correct():
     assert convert("1/2") == 50
 
-def test_convert_return_incorrect():
-    assert convert("1/2") == 
+def test_convert_return_empty():
+    assert convert("1/100") == "E"
+
+def test_convert_return_full():
+    assert convert("1/1") == "F"
 
 def test_gauge_return():
     assert gauge(50) == "50%"
