a37b7dd91dbd4a7db2015538f1bb9ef87bd0c8e7
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-21 20:36:52 +0000

    Wed, Jun 21, 2023, 4:36 PM -04:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,16 +1,20 @@
 def main():
     while True:
-        try:
-            fraction = input("Fraction: ").strip()
-            fraction = convert(fraction)
-            print(gauge(fraction))
-            break
+        fraction = input("Fraction: ").strip()
+        fraction = convert(fraction)
+        print(gauge(fraction))
+        break
 
 def convert(fraction):
     # convert in percentage
     try:
         x, y = fraction.split('/')
         x, y = int(x), int(y)
+
+        # prompt again if x > y
+        if (x > y):
+            continue
+
     except(ValueError, ZeroDivisionError):
         pass
     else:
