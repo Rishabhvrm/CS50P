bca205f1199476417dc9819a2afdee14e0be7414
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 21:05:46 +0000

    Sat, Jun 24, 2023, 5:05 PM -04:00

--- 2023/Lecture-6-FileIO/7costumes.py
+++ 2023/Lecture-6-FileIO/7costumes.py
@@ -2,3 +2,16 @@
 # PIL - Python Image Library
 # pillow.readthedocs.io
 
+import sys
+
+from PIL import Image
+
+images = []
+
+for arg in sys.argv:
+    image = Image.open(arg)
+    images.append(image)
+
+images[0].save(
+    "img1", save
+)
