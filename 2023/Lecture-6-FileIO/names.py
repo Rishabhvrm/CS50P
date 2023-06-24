80044f822b8a0dddf6f6d7da3f7bffb42559dd63
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 17:21:03 +0000

    Sat, Jun 24, 2023, 1:21 PM -04:00

--- 2023/Lecture-6-FileIO/names.py
+++ 2023/Lecture-6-FileIO/names.py
@@ -23,13 +23,15 @@ with open("2names.txt", "a") as file:
 #         print("hello,", line.rstrip())
 
 # part 4, read file and sort
-names = []
-with open("1names.txt") as file:
-    for line in file:
-        names.append(line.rstrip())
+# names = []
+# with open("1names.txt") as file:
+#     for line in sorted:
+#         names.append(line.rstrip())
 
-for name in sorted(names):
-    print(f"hello, {name}")
+# for name in sorted(names):
+#     print(f"hello, {name}")
+
+# part 5, use sorted() on file
 
 # -------------------------
 
@@ -37,3 +39,4 @@ for name in sorted(names):
 # returns a file handle - allows us to access a file
 # use 'with' - no need to close explicitly, more pythonic
 # rstrip - strip the string
+# part 4 - a common way to do things in python, collect while reading the file and then late manipulate it
