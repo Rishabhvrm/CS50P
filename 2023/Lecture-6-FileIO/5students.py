a9d77414b6820202d9e4217bc9e73faf2fc46c9d
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 20:41:01 +0000

    Sat, Jun 24, 2023, 4:41 PM -04:00

--- 2023/Lecture-6-FileIO/5students.py
+++ 2023/Lecture-6-FileIO/5students.py
@@ -8,6 +8,6 @@ with open("5students.csv") as file:
     reader = csv.DictReader(file)   # load dictionary of columns, returns dictionary
     for row in reader:
         students.append({"name": row["name"], "home": row["home"]})
-
+print(students)
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is from {student['home']}")
