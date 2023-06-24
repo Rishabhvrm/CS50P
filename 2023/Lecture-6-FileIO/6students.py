23275bf0a5f6a9edcd8af48b37752ff58dc4f74f
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 20:54:46 +0000

    Sat, Jun 24, 2023, 4:54 PM -04:00

--- 2023/Lecture-6-FileIO/6students.py
+++ 2023/Lecture-6-FileIO/6students.py
@@ -1,11 +1,18 @@
 # write a csv
 
+
+
 import csv
 
 name = input("Name: ")
 home = input("Home: ")
 
+# Part A - using writer
+# with open("6students.csv", "a") as file:
+#     writer = csv.writer(file)
+#     writer.writerow([name, home])   # write a list of name and home
+
+# Part B - using DictWriter
 with open("6students.csv", "a") as file:
-    writer = csv.writer(file)
-    writer.writerow([name, home])   # write a list of name and home
-    
+    writer = csv.DictWriter(file, fieldnames = ["name", "home"])
+    writer.writerow({"name": name, "home": home}) # provide a dictionary
