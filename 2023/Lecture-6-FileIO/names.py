4b7c6f42e36031eee84076fd1e556d5e56bb978a
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 16:42:06 +0000

    Sat, Jun 24, 2023, 12:42 PM -04:00

--- 2023/Lecture-6-FileIO/names.py
+++ 2023/Lecture-6-FileIO/names.py
@@ -1,6 +1,7 @@
 name = input("What's your name? ")
 
-file = open("1names.txt", "w")
+# file = open("1names.txt", "w")    # write - it will recreate the file everytime this is called. Hence overwriting new info over old info
+file = open("1names.txt", "a")      # append
 file.write(name)
 file.close()    # close and save the file
 
