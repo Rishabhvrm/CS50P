4521996450d80568da0d2ede5e151a386a87b91d
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 21:11:50 +0000

    Sat, Jun 24, 2023, 5:11 PM -04:00

--- 2023/Lecture-6-FileIO/7costumes.py
+++ 2023/Lecture-6-FileIO/7costumes.py
@@ -6,11 +6,12 @@ import sys
 
 from PIL import Image
 
-images = []
+images = ['https://www.shutterstock.com/image-photo/young-woman-canoeing-lake-bohinj-on-1494248774',
+          'https://www.shutterstock.com/image-photo/multiracial-male-female-friends-standing-talking-2268291185']
 
-for arg in sys.argv[1:]:
-    image = Image.open(arg)
-    images.append(image)
+# for arg in sys.argv[1:]:
+#     image = Image.open(arg)
+#     images.append(image)
 
 images[0].save(
     "img1.gif", save_all=True,
