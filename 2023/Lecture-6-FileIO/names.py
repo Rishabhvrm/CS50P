b0e05d865ab663c431e10ae6f94e09be256e19cf
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 16:55:26 +0000

    Sat, Jun 24, 2023, 12:55 PM -04:00

--- 2023/Lecture-6-FileIO/names.py
+++ 2023/Lecture-6-FileIO/names.py
@@ -1,12 +1,26 @@
 name = input("What's your name? ")
 
+# part 1, 'open'
 # file = open("1names.txt", "w")    # write - it will recreate the file everytime this is called. Hence overwriting new info over old info
 file = open("1names.txt", "a")      # append
 file.write(f"{name}\n")
 file.close()    # close and save the file
 
+
+# part 2, 'with'
+with open("2names.txt", "a") as file:
+    file.write(f"{name}\n")
+
+# part 3, read file
+
+with open("1names.txt", "r") as file:
+    lines = file.readlines()
+
+for line in lines:
+    print("hello", line)
+
 # -------------------------
 
 # open - like double clicking a file, second argument - what do you want to do this with file (read/write)
 # returns a file handle - allows us to access a file
-# use 'with' - no need to close explicitly
+# use 'with' - no need to close explicitly, more pythonic
