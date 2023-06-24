5b36a1d4a35437cc4ff16f9da5f419c77540f194
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 16:39:10 +0000

    Sat, Jun 24, 2023, 12:39 PM -04:00

--- 2023/Lecture-6-FileIO/names.py
+++ 2023/Lecture-6-FileIO/names.py
@@ -1,14 +1,10 @@
-names = []
+name = input("What's your name? ")
 
-for _ in range(3):
-    names.append(input("Enter a name: "))
-
-for name in sorted(names):
-    print(f"hello, {name}")
-
-print(names)
-
+file = open("1names.txt", "w")
+file.write(name)
+file.close()    # close and save the file
 
 # -------------------------
 
-# open - like double clicking a file
+# open - like double clicking a file, second argument - what do you want to do this with file (read/write)
+# returns a file handle - allows us to access a file
