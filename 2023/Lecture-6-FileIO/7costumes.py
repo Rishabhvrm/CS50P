961e67b7027a11a92d912064009eaa4bbf6a63eb
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 21:08:31 +0000

    Sat, Jun 24, 2023, 5:08 PM -04:00

--- 2023/Lecture-6-FileIO/7costumes.py
+++ 2023/Lecture-6-FileIO/7costumes.py
@@ -8,10 +8,11 @@ from PIL import Image
 
 images = []
 
-for arg in sys.argv:
+for arg in sys.argv[1:]:
     image = Image.open(arg)
     images.append(image)
 
 images[0].save(
-    "img1", save
+    "img1.gif", save_all=True,
+    append_images=[images[1]], duration=200, loop=0
 )
