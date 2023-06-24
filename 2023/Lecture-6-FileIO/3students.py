44a786f1e85a7cecff9b3a180fbc8292ce66a0c5
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 18:04:55 +0000

    Sat, Jun 24, 2023, 2:04 PM -04:00

--- 2023/Lecture-6-FileIO/3students.py
+++ 2023/Lecture-6-FileIO/3students.py
@@ -5,24 +5,20 @@ with open("3students.csv") as file:
    for line in file:
       name, house = line.rstrip().split(",")
       student = {}                      # use this for line 7, 8, 9 -> student = {"name": name, "house": house}
-    #   student["name"] = name
-    #   student["house"] = house
-      student[name] = house
+      student["name"] = name
+      student["house"] = house
       students.append(student)
 
 print(students)
 
 def get_name(student):
-   return student[0]
+   return student['name']
 
-# for student in sorted(students, key = get_name):
-#    print(f"{student['name']} is in {student['house']}")
-
-for student in sorted(students, key = student.items()):
-   for k,v in student.items():
-      print(k,v)
+for student in sorted(students, key=get_name, reverse=True):
+   print(f"{student['name']} is in {student['house']}")
 
 
 # read csv and sort while reading the file
 # declaring a dict - student = {"name": name, "house": house}
 # functions as arguments into other functions
+# sorting a list of dictionaries
