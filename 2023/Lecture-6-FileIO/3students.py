368ec1ceab75c0c92e6fef658dc9084ed6ac5f54
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 18:13:12 +0000

    Sat, Jun 24, 2023, 2:13 PM -04:00

--- 2023/Lecture-6-FileIO/3students.py
+++ 2023/Lecture-6-FileIO/3students.py
@@ -9,16 +9,18 @@ with open("3students.csv") as file:
       student["house"] = house
       students.append(student)
 
-print(students)
+# def get_name(student):
+#    return student['name']
 
-def get_name(student):
-   return student['name']
+# refer code in next section to use lambda function to call get_name()
+# for student in sorted(students, key=get_name, reverse=True):
+#    print(f"{student['name']} is in {student['house']}")
 
-for student in sorted(students, key=get_name, reverse=True):
-   print(f"{student['name']} is in {student['house']}")
-
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['house']}")
 
 # read csv and sort while reading the file
 # declaring a dict - student = {"name": name, "house": house}
 # functions as arguments into other functions
 # sorting a list of dictionaries
+# lambda function
