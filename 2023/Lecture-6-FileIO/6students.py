934fa9aa40664df983658c8b2631f44c9f7296bf
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-24 20:57:03 +0000

    Sat, Jun 24, 2023, 4:57 PM -04:00

--- 2023/Lecture-6-FileIO/6students.py
+++ 2023/Lecture-6-FileIO/6students.py
@@ -14,5 +14,5 @@ home = input("Home: ")
 
 # Part B - using DictWriter
 with open("6students.csv", "a") as file:
-    writer = csv.DictWriter(file, fieldnames = ["name", "home"])  # order of columns
+    writer = csv.DictWriter(file, fieldnames = ["name", "home"])  # order of colu mns
     writer.writerow({"name": name, "home": home}) # provide a dictionary
