8637e5ddc653c07ab32bbbe6bdb8c810d6a3c828
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-26 21:59:32 +0000

    Mon, Jun 26, 2023, 5:59 PM -04:00

--- lines/lines.py
+++ lines/lines.py
@@ -2,19 +2,26 @@ import sys
 
 count = 0
 
-if len(argv < 2):
+if len(sys.argv) < 2:
     sys.exit('Too few command-line arguments')
 
-if len(argv > 2):
+if len(sys.argv) > 2:
     sys.exit('Too many command-line arguments')
 
-if not argv[1].endswith('.py'):
+if not sys.argv[1].endswith('.py'):
     sys.exit('Not a Python file')
 
-with open('sample.py') as file:
+
+try:
+    with open('sample.py') as file:
+        print(line)
+except FileNotFoundError:
+    sys.exit('File does not exist')
+else:
     for line in file:
         if not line.startswith('#') and line.strip() != "":
             print('start' + line + 'end')
             count += 1
+
 
 print(count)
