faeddf4d4a1da8f37ce4360a52ea4f1cf581e86d
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-26 21:47:26 +0000

    Mon, Jun 26, 2023, 5:47 PM -04:00

--- lines/lines.py
+++ lines/lines.py
@@ -2,8 +2,8 @@ count = 0
 
 with open('sample.py') as file:
     for line in file:
-        if not line.startswith('#') and line.strip != "":
-            print("yep")
+        if line.startswith('#') or line.strip != "":
+            print('start' + line + 'end')
             count += 1
 
 print(count)
