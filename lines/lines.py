0529daf8cd94348bb92e37acb0cf92de3f024ca3
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-26 22:25:45 +0000

    Mon, Jun 26, 2023, 6:25 PM -04:00

--- lines/lines.py
+++ lines/lines.py
@@ -14,6 +14,7 @@ if not sys.argv[1].endswith('.py'):
 count = 0
 try:
     with open(sys.argv[1]) as file:
+        # check for comment and whitespaces
         for line in file:
             if not line.strip().startswith('#') and line.strip() != "": count += 1
 except FileNotFoundError:
