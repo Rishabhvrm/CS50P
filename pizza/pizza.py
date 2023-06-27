d71d3eed6520639f2098cf666d0b915d0321bf18
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-27 16:50:47 +0000

    Tue, Jun 27, 2023, 12:50 PM -04:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 
 # cmd line argument checks
 if len(sys.argv) < 2:
@@ -7,8 +8,13 @@ if len(sys.argv) < 2:
 if len(sys.argv) > 2:
     sys.exit('Too many command-line arguments')
 
-if not sys.argv[1].endswith('.py'):
-    sys.exit('Not a Python file')
+if not sys.argv[1].endswith('.csv'):
+    sys.exit('Not a CSV file')
 
 with open(sys.argv[1]) as file:
-    
+    try:
+        reader = csv.DictReader(file)
+        for row in reader:
+            print(row)
+    except FileNotFoundError:
+        sys.exit('File does not exist')
