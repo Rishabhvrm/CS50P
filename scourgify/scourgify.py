fecdee550a49e6f27a773ee07105f723419bbc43
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-27 18:01:04 +0000

    Tue, Jun 27, 2023, 2:01 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,5 +1,15 @@
 import csv
 import sys
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     sys.exit('Too few command-line arguments')
+
+if len(sys.argv) > 3:
+    sys.exit('Too many command-line arguments')
+
+
+try:
+    with open(sys.argv[1]) as file:
+
+except FileNotFoundError:
+    sys.exit(')
