6d8b7b37e1997c3c27c8704560fff1da2e5cbad7
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-27 18:05:55 +0000

    Tue, Jun 27, 2023, 2:05 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -7,11 +7,19 @@ if len(sys.argv) < 3:
 if len(sys.argv) > 3:
     sys.exit('Too many command-line arguments')
 
+if not sys.argv[1].endswith('.csv'):
+    sys.exit(f'{sys.argv[1]} is not a valid CSV file')
 
+if not sys.argv[2].endswith('.csv'):
+    sys.exit(f'{sys.argv[2]} is not a valid CSV file')
+
+students =
 try:
     with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         print(reader)
+        for row in reader:
+            students.append({name: row['name'], house: row['house']})
 
 except FileNotFoundError:
     sys.exit(f'Could not read {sys.argv[1]}')
