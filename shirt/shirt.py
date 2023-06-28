d0c67303eeb66db0dede88e392c86880097f77fe
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-28 13:53:43 +0000

    Wed, Jun 28, 2023, 9:53 AM -04:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -18,6 +18,8 @@ else:
     valid_extensions = ['.jpg', '.jpeg', '.png']
     if extension1 not in valid_extensions or extension2 not in valid_extension:
         print('------' + extension1 + '-----' + extension2)
+        print(extension1 not in valid_extensions)
+        # print(extension2 not in valid_extension)
         sys.exit('Invalid output')
     if extension1 != extension2:
         sys.exit('Input and output have different extensions')
