358a219131a582bc5f5ca837980822a1468898cf
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-28 21:32:15 +0000

    Wed, Jun 28, 2023, 5:32 PM -04:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -16,7 +16,7 @@ def main():
         # resize input to the size of shirt
         muppet_resized_img = ImageOps.fit(muppet, shirt_size, method=Image.Resampling.BICUBIC, bleed=0.0, centering=(0.5, 0.5))
         muppet_resized_img.paste(shirt, shirt)      # overlay shirt over muppet
-        muppet_resized_img.save(sys.argv[2])        # save it
+        muppet_resized_img.save(sys.argv[2])        # save resized muppet with name given in cmd
 
 
 def validations():
@@ -25,19 +25,18 @@ def validations():
 
     if len(sys.argv) > 3:
         sys.exit('Too many command-line arguments')
-    _, extension1 = os.path.splitext(sys.argv[1])
-    # try:
-    #     _, extension1 = os.path.splitext(sys.argv[1])
-    #     _, extension2 = os.path.splitext(sys.argv[2])
-    # except:
-    #     sys.exit('Invalid output')                      # if filename does not have '.'
-    # else:
-    #     valid_extension = ['.jpg', '.jpeg', '.png']
-    #     if extension1 not in valid_extension:
-    #         sys.exit('Invalid input')
-    #     if extension2 not in valid_extension:
-    #         sys.exit('Invalid output')
-    #     if extension1 != extension2:
-    #         sys.exit('Input and output have different extensions')
+    try:
+        _, extension1 = os.path.splitext(sys.argv[1])
+        _, extension2 = os.path.splitext(sys.argv[2])
+    except:
+        sys.exit('Invalid output')                      # if filename does not have '.'
+    else:
+        valid_extension = ['.jpg', '.jpeg', '.png']
+        if extension1 not in valid_extension:
+            sys.exit('Invalid input')
+        if extension2 not in valid_extension:
+            sys.exit('Invalid output')
+        if extension1 != extension2:
+            sys.exit('Input and output have different extensions')
 
 main()
