112e37ef2804b77bc3af39fffb9e8c1650f685c8
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-28 14:02:27 +0000

    Wed, Jun 28, 2023, 10:02 AM -04:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,28 +1,6 @@
 import sys
 from PIL import Image
 
-if len(sys.argv) < 3:
-    sys.exit('Too few command-line arguments ')
-
-if len(sys.argv) > 3:
-    sys.exit('Too many command-line arguments')
-
-
-try:
-    _, extension1 = sys.argv[1].split('.')
-    __, extension2 = sys.argv[2].split('.')
-    print(extension1, extension2)
-except:
-    sys.exit('Invalid output')      # if filename does not have '.'
-else:
-    valid_extension = ['jpg', 'jpeg', 'png']
-    if extension1 not in valid_extension or extension2 not in valid_extension:
-        print('------' + extension1 + '-----' + extension2)
-        print(extension1 not in valid_extension)
-        print(extension2 not in valid_extension)
-        sys.exit('Invalid output')
-    if extension1 != extension2:
-        sys.exit('Input and output have different extensions')
 
 images = []
 
@@ -34,3 +12,25 @@ except FileNotFoundError:
     sys.exit('Input does not exist')
 
 
+def validations():
+    if len(sys.argv) < 3:
+    sys.exit('Too few command-line arguments ')
+
+    if len(sys.argv) > 3:
+        sys.exit('Too many command-line arguments')
+
+    try:
+        _, extension1 = sys.argv[1].split('.')
+        __, extension2 = sys.argv[2].split('.')
+        print(extension1, extension2)
+    except:
+        sys.exit('Invalid output')      # if filename does not have '.'
+    else:
+        valid_extension = ['jpg', 'jpeg', 'png']
+        if extension1 not in valid_extension or extension2 not in valid_extension:
+            print('------' + extension1 + '-----' + extension2)
+            print(extension1 not in valid_extension)
+            print(extension2 not in valid_extension)
+            sys.exit('Invalid output')
+        if extension1 != extension2:
+            sys.exit('Input and output have different extensions')
