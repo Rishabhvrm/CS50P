c7bc66b2d1b13a7101c73e61f44da3343f646388
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-28 21:20:05 +0000

    Wed, Jun 28, 2023, 5:20 PM -04:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -26,18 +26,18 @@ def validations():
     if len(sys.argv) > 3:
         sys.exit('Too many command-line arguments')
     _, extension1 = os.path.splitext(sys.argv[1])
-    try:
-        _, extension1 = os.path.splitext(sys.argv[1])
-        _, extension2 = os.path.splitext(sys.argv[2])
-    except:
-        sys.exit('Invalid output')                      # if filename does not have '.'
-    else:
-        valid_extension = ['.jpg', '.jpeg', '.png']
-        if extension1 not in valid_extension:
-            sys.exit('Invalid input')
-        if extension2 not in valid_extension:
-            sys.exit('Invalid output')
-        if extension1 != extension2:
-            sys.exit('Input and output have different extensions')
+    # try:
+    #     _, extension1 = os.path.splitext(sys.argv[1])
+    #     _, extension2 = os.path.splitext(sys.argv[2])
+    # except:
+    #     sys.exit('Invalid output')                      # if filename does not have '.'
+    # else:
+    #     valid_extension = ['.jpg', '.jpeg', '.png']
+    #     if extension1 not in valid_extension:
+    #         sys.exit('Invalid input')
+    #     if extension2 not in valid_extension:
+    #         sys.exit('Invalid output')
+    #     if extension1 != extension2:
+    #         sys.exit('Input and output have different extensions')
 
 main()
