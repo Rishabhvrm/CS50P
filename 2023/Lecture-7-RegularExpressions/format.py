07caa6b37f41a82d8522eb852833f34f59282003
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-30 15:57:22 +0000

    Fri, Jun 30, 2023, 11:57 AM -04:00

--- 2023/Lecture-7-RegularExpressions/format.py
+++ 2023/Lecture-7-RegularExpressions/format.py
@@ -1,7 +1,27 @@
 # clean up or format user input data
 
+import re
+
 name = input("What's your name? ").strip()
-if "," in name:
-    last, first = name.split(", ")
+# matches = re.search(r"^.+, .+$", name)
+matches = re.search(r"^(.+), (.+)$", name)          # not grouping by using (), we want a return value, we want to capture something
+if matches:                                         # i.e. we found something, matches is not empty
+    last, first = matches.groups()
     name = f"{first} {last}"
 print(f"hello, {name}")
+
+
+"""
+
+NOTES:
+
+we can extract info from re
+whatever matches in paranthesis, can be returned
+
+(?:...) don't capture, just group
+
+
+get a return value for when grouped
+use it using matches.groups()    # as seen in above code
+
+"""
