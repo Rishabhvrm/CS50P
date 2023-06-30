39a5621644d7931402d4692ba6f1922c2d375d42
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-30 16:00:25 +0000

    Fri, Jun 30, 2023, 12:00 PM -04:00

--- 2023/Lecture-7-RegularExpressions/format.py
+++ 2023/Lecture-7-RegularExpressions/format.py
@@ -5,7 +5,7 @@ import re
 name = input("What's your name? ").strip()
 # matches = re.search(r"^.+, .+$", name)
 matches = re.search(r"^(.+), ?(.+)$", name)          # not grouping by using (), we want a return value, we want to capture something
-if matches:                                         # i.e. we found something, matches is not empty
+if matches:                                          # i.e. we found something, matches is not empty
     last = matches.group(1)
     first = matches.group(2)
     name = f"{first} {last}"
