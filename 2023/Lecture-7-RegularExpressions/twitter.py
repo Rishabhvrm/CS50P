f837bf93b015300d7098a7fe73c95a3a074c36e6
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-30 16:40:28 +0000

    Fri, Jun 30, 2023, 12:40 PM -04:00

--- 2023/Lecture-7-RegularExpressions/twitter.py
+++ 2023/Lecture-7-RegularExpressions/twitter.py
@@ -2,9 +2,15 @@
 # prompt user for twitter url
 # extract user name from that
 
+import re
+
 url = input("URL: ").strip()
 # username = url.replace("https://twitter.com/", "") # replace starting url with nothing
-username = url.removeprefix("https://twitter.com/")
+# username = url.removeprefix("https://twitter.com/")
+
+# substitute
+re.sub(r"https://twitter.com/", "", url)
+
 print(f"Username: {username}")
 
 # https://twitter.com/davidjmalan
@@ -12,7 +18,10 @@ print(f"Username: {username}")
 
 """
 
-using
+using string methods
     replace()
     removeprefix()
+
+using re method
+    re.sub(pattern, repl, string, count=0, flags=0)     # substitue, replacement
 """
