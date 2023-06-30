bbb02e07db5186ccc34d3b23a5c9853305788c36
Rishabh Verma <rishabhvrm43@gmail.com>
2023-06-30 17:30:08 +0000

    Fri, Jun 30, 2023, 1:30 PM -04:00

--- 2023/Lecture-7-RegularExpressions/twitter.py
+++ 2023/Lecture-7-RegularExpressions/twitter.py
@@ -14,7 +14,7 @@ url = input("URL: ").strip()
 # (.+) is for return value, we used (www.) for grouping but it also returns a value
 # if matches := re.search(r"^https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):  print(f"Username:", matches.group(1))
 # only take data that is needed, we don't need www in above
-if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):     # here we specify that we don't need www. therefore it will not return its value and we can get the value in matches.group(1) instead of matches.group(2)
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(\w+)", url, re.IGNORECASE):     # here we specify that we don't need www. therefore it will not return its value and we can get the value in matches.group(1) instead of matches.group(2)
     print(f"Username:", matches.group(1)) # not matches.group(2) now because of (?:www\.)
 
 # https://twitter.com/davidjmalan
