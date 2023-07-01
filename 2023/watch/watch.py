32e7f5bd12c4c8f1eb46e90541e7c7f5150cebab
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-01 18:49:30 +0000

    Sat, Jul 1, 2023, 2:49 PM -04:00

--- 2023/watch/watch.py
+++ 2023/watch/watch.py
@@ -3,11 +3,9 @@ import re
 def main():
     print(parse(input("HTML: ")))
 
-
 def parse(s):
     if matches := re.search(r"^iframe.*https?://(?:www\.)?youtube\.com/embed/(\w+)", s, re.IGNORECASE):
         return("https://youtu.be/" + matches.group(1))
-
 
 if __name__ == "__main__":
     main()
