046908a1083e08fe4e093876a261a23f1dc799b6
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-01 17:30:31 +0000

    Sat, Jul 1, 2023, 1:30 PM -04:00

--- 2023/watch/watch.py
+++ 2023/watch/watch.py
@@ -5,8 +5,8 @@ def main():
 
 
 def parse(s):
-    re.search(r"srch=.+", s, flag=re.IGNORECASE)
-
+    if matches := re.search(r"^https?://(?:www\.)?youtube\.com/embed/(\w+)", s, flag=re.IGNORECASE):
+        print(matches.group(1))
 
 
 if __name__ == "__main__":
