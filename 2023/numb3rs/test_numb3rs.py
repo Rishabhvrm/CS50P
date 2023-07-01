b18a065741fe3957410d8641000e9274647dd101
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-01 16:23:06 +0000

    Sat, Jul 1, 2023, 12:23 PM -04:00

---
+++ 2023/numb3rs/test_numb3rs.py
@@ -0,0 +1,10 @@
+import pytest
+
+def test_five_digit_ip():
+    assert validate('1.2.3.4.5') == False
+
+def test_out_of_range():
+    assert validate('300.1.2.3') == False
+
+def test_in_range():
+    assert validate('1.2.3.4') == True
