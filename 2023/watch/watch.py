c4436f0b2eb8609240005a19ae876d947afdcb94
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-01 17:24:38 +0000

    Sat, Jul 1, 2023, 1:24 PM -04:00

--- 2023/watch/watch.py
+++ 2023/watch/watch.py
@@ -4,9 +4,13 @@ def main():
     print(parse(input("HTML: ")))
 
 
-def parse():
+def parse(s):
+    re.search(r"", s, flag=re.IGNORECASE)
 
 
 
 if __name__ == "__main__":
     main()
+
+
+# src="https://www.youtube.com/embed/xvFZjo5PgG0" title="Y
