94f76d8ed9bd258265cad1a8562b82343abdc29e
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-01 14:59:01 +0000

    Sat, Jul 1, 2023, 10:59 AM -04:00

--- 2023/numb3rs/numb3rs.py
+++ 2023/numb3rs/numb3rs.py
@@ -11,7 +11,7 @@ def main():
 
 
 def validate(ip):
-    regex = "^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$"
+    regex = "([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
     if re.search(rf"{regex}\.{regex}\.{regex}\.{regex}", ip):
         return True
     else:
