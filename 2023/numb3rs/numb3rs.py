5a336c7fdd439cb82b637145e95e13bc8a341e1b
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-01 16:17:29 +0000

    Sat, Jul 1, 2023, 12:17 PM -04:00

--- 2023/numb3rs/numb3rs.py
+++ 2023/numb3rs/numb3rs.py
@@ -5,9 +5,11 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 def validate(ip):
-    # 
+    #
     regex = "([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"
-    return re.search(rf"^{regex}\.{regex}\.{regex}\.{regex}$", ip)
+    # return True if re.search(rf"^{regex}\.{regex}\.{regex}\.{regex}$", ip) else False
+    matches = re.search(rf"^{regex}\.{regex}\.{regex}\.{regex}$", ip)
+    print(matches.group(1), matches.group(2))
 
 if __name__ == "__main__":
     main()
