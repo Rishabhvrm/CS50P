6343dc04df7710f910a80983cb2db7c45e4fa9df
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-02 21:50:45 +0000

    Sun, Jul 2, 2023, 5:50 PM -04:00

--- 2023/working/working.py
+++ 2023/working/working.py
@@ -7,9 +7,11 @@ def main():
 # 9:00 AM to 5:00 PM
 # 9 AM to 5 PM
 def convert(time):
-    if (re.search(r"^\d{1,2}:\d{1,2} AM to \d{1,2}:\d{1,2} PM$", time)):
+    if matches := re.search(r"^(\d{1,2}):(\d{1,2}) AM to (\d{1,2}):(\d{1,2}) PM$", time)):
         print('Yayy')
-    else: print('Nayy')
+        h_am, m_am, h_pm, m_pm = matches.group(1), matches.group(2), matches.group(3), matches.group(4)
+    elif matches := re.search(r"", time)
+    print('Nayy')
 
 
 
