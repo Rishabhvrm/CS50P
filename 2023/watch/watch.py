9b0538d852cbf802ae264d042219bae49f3427c6
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-02 20:50:37 +0000

    Sun, Jul 2, 2023, 4:50 PM -04:00

--- 2023/watch/watch.py
+++ 2023/watch/watch.py
@@ -5,6 +5,11 @@ def main():
 
 def parse(s):
     # starts with <
+    # may have 0 or more characters after that
+    # must have http, s is optional (? is 0 or 1)
+    # must have ://
+    # www. is optional (? is 0 or 1)
+    # must have youtube.com/embed/, add \ before . to let the regex know we want dot(.) not any other character
     
     if matches := re.search(r"<.*https?://(?:www\.)?youtube\.com/embed/(\w+)", s, re.IGNORECASE):
         return("https://youtu.be/" + matches.group(1))
