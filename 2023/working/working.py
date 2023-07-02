eea3c4f9e36ae0ba45666b797cbb60f5a36ee201
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-02 21:32:16 +0000

    Sun, Jul 2, 2023, 5:32 PM -04:00

--- 2023/working/working.py
+++ 2023/working/working.py
@@ -1,11 +1,15 @@
 import re
 
 def main():
-
     print(convert(input("Hours: ")))
 
+# allowed input
+# 9:00 AM to 5:00 PM
+# 9 AM to 5 PM
+def convert(time):
+    if ()
 
-def convert():
+
 
 
 if __name__ == "__main__":
