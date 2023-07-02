2c153c9074cd9814aebcc04983ea088ef3847079
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-02 22:29:44 +0000

    Sun, Jul 2, 2023, 6:29 PM -04:00

--- 2023/working/working.py
+++ 2023/working/working.py
@@ -10,22 +10,25 @@ def convert(time):
     if matches := re.search(r"^(\d{1,2}):(\d{1,2}) (AM|PM) to (\d{1,2}):(\d{1,2}) (AM|PM)$", time):
         print('Yayy')
         hour, minute, time_indicator = matches.group(1), matches.group(2), matches.group(3)
-        start = convert_to_military_time(hour, minute, time_indicator)
+        start = convert_to_military_time(time_indicator, hour, minute)
 
         hour, minute, time_indicator = matches.group(4), matches.group(5), matches.group(6)
-        end = convert_to_military_time(hour, minute, time_indicator)
+        end = convert_to_military_time(time_indicator, hour, minute)
+
         return f"{start} to {end}"
 
     elif matches := re.search(r"^(\d{1,2}) (AM|PM) to (\d{1,2}) (AM|PM)$", time):
         print('Nayy')
         hour, time_indicator = matches.group(1), matches.group(2)
-        convert_to_military_time(h_am, h_pm)
+        start = convert_to_military_time(time_indicator, hour)
 
-        hour, time_indicator = matches.group(3), matches.group(2)
-        convert_to_military_time(h_am, h_pm)
+        hour, time_indicator = matches.group(3), matches.group(4)
+        end = convert_to_military_time(time_indicator, hour)
+
+        return f"{start} to {end}"
 
 
-def convert_to_military_time(h, m="00", time_indicator):
+def convert_to_military_time(time_indicator, h, m="00"):
     if time_indicator == "PM":
         h = str(int(h) + 12)
     return f"{h}:{m}"
