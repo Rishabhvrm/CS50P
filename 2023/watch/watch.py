cb5cc4c56564015e1f75f09f3f63c76a839e2ee7
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-02 20:48:14 +0000

    Sun, Jul 2, 2023, 4:48 PM -04:00

--- 2023/watch/watch.py
+++ 2023/watch/watch.py
@@ -4,6 +4,8 @@ def main():
     print(parse(input("HTML: ")))
 
 def parse(s):
+    # starts with <
+    
     if matches := re.search(r"<.*https?://(?:www\.)?youtube\.com/embed/(\w+)", s, re.IGNORECASE):
         return("https://youtu.be/" + matches.group(1))
 
