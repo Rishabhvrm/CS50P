4a3a52e24f200f98c3978b069eecbaf568174613
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-02 21:38:41 +0000

    Sun, Jul 2, 2023, 5:38 PM -04:00

--- 2023/working/working.py
+++ 2023/working/working.py
@@ -7,7 +7,7 @@ def main():
 # 9:00 AM to 5:00 PM
 # 9 AM to 5 PM
 def convert(time):
-    if (re.search(r"^\d{12}:\d{12} AM to \d{12}:\d{12} PM", time)):
+    if (re.search(r"^\d{12}:\d{12} AM to \d{12}:\d{12} PM$", time)):
         print('Yayy')
     else: print('Nayy')
 
