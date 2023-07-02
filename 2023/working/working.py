b437c5e5b7d3e924057603ccefd71e6c2d2fd5ef
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-02 22:14:22 +0000

    Sun, Jul 2, 2023, 6:14 PM -04:00

--- 2023/working/working.py
+++ 2023/working/working.py
@@ -11,7 +11,7 @@ def convert(time):
         # print('Yayy')
         h_am, m_am, h_pm, m_pm = matches.group(1), matches.group(2), matches.group(3), matches.group(4)
         return convert_to_military_time(h_am, h_pm, m_am, m_pm)
-    elif matches := re.search(r"^(\d{1}) AM to (\d{1}) PM$", time):
+    elif matches := re.search(r"^(\d{1}) (?:AM|PM) to (\d{1}) (?:AM|PM)$", time):
         # print('Nayy')
         h_am, h_pm = matches.group(1), matches.group(2)
         return convert_to_military_time(h_am, h_pm)
