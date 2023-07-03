95ab56ebc073dabe3bb9072d8f80f9a381b52b33
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-03 22:15:59 +0000

    Mon, Jul 3, 2023, 6:15 PM -04:00

--- 2023/working/test_working.py
+++ 2023/working/test_working.py
@@ -6,3 +6,9 @@ def test_hours():
 
 def test_hours_incorrect():
     assert convert("12 AM to 12 PM") != "01:00 to 13:00"
+
+def test_minutes_incorrect():
+    assert convert("9:00 AM to 5:00 PM") != "09:05 to 17:00"
+
+def test_format():
+    assert convert("") != ""
