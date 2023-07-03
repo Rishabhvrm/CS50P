4f7e09db26752fb6e3150633c67d160bc9b548ea
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-03 21:44:18 +0000

    Mon, Jul 3, 2023, 5:44 PM -04:00

--- 2023/working/working.py
+++ 2023/working/working.py
@@ -29,7 +29,7 @@ def convert(time):
 
 
 def convert_to_military_time(time_indicator, h, m="00"):
-    if time_indicator == "PM":
+    if time_indicator == "PM" and h != 12:
         h = str(int(h) + 12)
     if len(h) == 1:
         h = f"0{h}"
