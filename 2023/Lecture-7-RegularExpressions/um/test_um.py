7eee394ed571c26debb748ca861847268588c78c
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-12 02:14:06 +0000

    Tue, Jul 11, 2023, 10:14 PM -04:00

--- 2023/Lecture-7-RegularExpressions/um/test_um.py
+++ 2023/Lecture-7-RegularExpressions/um/test_um.py
@@ -1,5 +1,14 @@
 import count from um
 
 
-def test_count():
+def test_single_um():
     assert count("um") == 1
+
+def test_um_in_word():
+    assert count("yummy") == 0
+
+def test_multiple_ums():
+    assert count("um um um") == 3
+
+def test_ums_with_other_characters():
+    assert count("um, um. (um)") == 3
