6a319ed3838d82f31fda8631eec81c85f01edce8
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-12 23:30:25 +0000

    Wed, Jul 12, 2023, 7:30 PM -04:00

--- 2023/Lecture-8-ObjectOrientedProgramming/student.py
+++ 2023/Lecture-8-ObjectOrientedProgramming/student.py
@@ -1,12 +1,14 @@
 def main():
-    name, house = get_student()
-    print(f"{name} from {house}")
+    student = get_student()
+
+    print(f"{student[0]} from {student[1]}")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return name, house
+    return name, house          # we're actually returning one tuple
+    # return (name, house)      # same as above
 
 
 if __name__ == "__main__":
@@ -17,5 +19,6 @@ if __name__ == "__main__":
 Notes:
 
 return multiple values
+tuple - immutable, similar to list
 
 """
