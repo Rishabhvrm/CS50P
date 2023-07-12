36022550dac2234bb62805be9a12814986143d18
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-12 02:43:29 +0000

    Tue, Jul 11, 2023, 10:43 PM -04:00

--- 2023/Lecture-7-RegularExpressions/response/response.py
+++ 2023/Lecture-7-RegularExpressions/response/response.py
@@ -3,11 +3,8 @@ from validator_collection import validators
 def main():
     print(validate(input("What's your email address? ")))
 
-def validate():
-
-
-
-
+def validate(email):
+    print(validators.email(email))
 
 if __name__ == "__main__":
     main()
