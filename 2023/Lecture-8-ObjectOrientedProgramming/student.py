0bc27dca4f7f1e2306af151023089ba704125484
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-12 23:27:04 +0000

    Wed, Jul 12, 2023, 7:27 PM -04:00

--- 2023/Lecture-8-ObjectOrientedProgramming/student.py
+++ 2023/Lecture-8-ObjectOrientedProgramming/student.py
@@ -3,12 +3,20 @@ def main():
     house = get_house()
     print(f"{name} from {house}")
 
-def get_name():
-    return input("Name: ")
 
-def get_house():
-    return input("House: ")
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return name, house
 
 
 if __name__ == "__main__":
     main()
+
+
+"""
+Notes:
+
+return multiple values
+
+"""
