5c8a2b65452d5ceca7fc883e7537a1ecebfecca1
Rishabh Verma <rishabhvrm43@gmail.com>
2023-07-12 23:31:13 +0000

    Wed, Jul 12, 2023, 7:31 PM -04:00

--- 2023/Lecture-8-ObjectOrientedProgramming/student.py
+++ 2023/Lecture-8-ObjectOrientedProgramming/student.py
@@ -1,13 +1,12 @@
 def main():
     student = get_student()
-
     print(f"{student[0]} from {student[1]}")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return name, house          # we're actually returning one tuple
+    return name, house          # we're actually returning one tuple, not two values
     # return (name, house)      # same as above
 
 
