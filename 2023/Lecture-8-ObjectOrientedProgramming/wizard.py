c8414f88880be933a73f0ef2003537820e77a6f4
Rishabh Verma <rishabhvrm43@gmail.com>
2023-08-15 20:23:50 +0000

    Tue, Aug 15, 2023, 4:23 PM -04:00

--- 2023/Lecture-8-ObjectOrientedProgramming/wizard.py
+++ 2023/Lecture-8-ObjectOrientedProgramming/wizard.py
@@ -15,14 +15,18 @@ class Wizard:
 #         self.subject = subject
 
 class Student(Wizard):
-    def __init__(house):
+    def __init__(self,name, house):
+        super().__init__(name)
         self.house = house
 
 class Professor(Wizard):
-    def __init__(subject):
+    def __init__(self, name, subject):
+        super().__init__(name)
         self.subject = subject
 
-
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus", "Defense Against the Dark Arts")
 
 """
 
@@ -31,6 +35,8 @@ better to define a class wizard which will have common attributes like name into
 and then student and professor can inherit the wizard class
 
 class Child inherits from class Parent
- - definition => class Child(Parent):
+ definition => class Child(Parent):
+ super() => access the parent class
+ super().__init__ => access parent's class __init__() method
 
  """
