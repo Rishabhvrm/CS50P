6bec451eee75346134c7e8429bd3ff7eb939a6d8
Rishabh Verma <rishabhvrm43@gmail.com>
2023-08-15 19:34:24 +0000

    Tue, Aug 15, 2023, 3:34 PM -04:00

--- 2023/Lecture-8-ObjectOrientedProgramming/type.py
+++ 2023/Lecture-8-ObjectOrientedProgramming/type.py
@@ -1,5 +1,10 @@
 print(type(50))         # output: <class 'int'>
+print(type(50.1))
+
 print(type("hello"))
+
 print(type([]))
 print(type(list()))
-print(type(50.1))
+
+print(type({}))
+print(type(dict()))
