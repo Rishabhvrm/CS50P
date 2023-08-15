1504b62f52642e32feceaf09b624d6014e215890
Rishabh Verma <rishabhvrm43@gmail.com>
2023-08-15 19:49:24 +0000

    Tue, Aug 15, 2023, 3:49 PM -04:00

--- 2023/Lecture-8-ObjectOrientedProgramming/hat.py
+++ 2023/Lecture-8-ObjectOrientedProgramming/hat.py
@@ -2,16 +2,21 @@
 import random
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    # def __init__(self):
+    #     self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
-    def sort(self, name):
-        print(name, "is in", random.choice(self.houses))
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
-hat = Hat()
-hat.sort("Harry")
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
 
+# before class method
+# hat = Hat()
+# hat.sort("Harry")
 
+# after class method
+Hat.sort("Harry")
 
 """
 Class Method
@@ -19,4 +24,12 @@ Class Method
 @classmethod
     - this method is not an instance method, doesn't have access to 'self'
     - if there are no objects of that class
+    - when class is just a container for data and functionality that are somehow related
+    - pass 'cls' in the method
+    - no need to instantiate the class
+
+class variable
+    - eg: houses in above eg
+    - share the same copy among all the objects
+
 """
