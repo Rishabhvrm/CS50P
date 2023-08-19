0db5d0fc671af020aca901d06be4a35dc618ec11
Rishabh Verma <rishabhvrm43@gmail.com>
2023-08-19 04:13:20 +0000

    Sat, Aug 19, 2023, 12:13 AM -04:00

---
+++ 2023/Lecture-8-ObjectOrientedProgramming/seasons/seasons.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+# expects date in YYYY-MM-DD format
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+"""
+
+https://cs50.harvard.edu/python/2022/psets/8/seasons/
+
+
+
+"""
