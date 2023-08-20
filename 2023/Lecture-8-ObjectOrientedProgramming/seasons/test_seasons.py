50a53e7f676cb052b8093ca0f7da90450c730967
Rishabh Verma <rishabhvrm43@gmail.com>
2023-08-20 19:31:50 +0000

    Sun, Aug 20, 2023, 3:31 PM -04:00

--- 2023/Lecture-8-ObjectOrientedProgramming/seasons/test_seasons.py
+++ 2023/Lecture-8-ObjectOrientedProgramming/seasons/test_seasons.py
@@ -3,5 +3,8 @@ from seasons import calculate_minutes
 def test_today():
     assert("2023-08-20") == "Zero minutes"
 
-def test_future():
-    assert("2024-08-20") == ""
+def test_future_year():
+    assert("2024-08-20") == "Minus five hundred twenty-seven thousand forty minutes"
+
+def test_past_year():
+    assert("2022-08-20") == "Five hundred twenty-seven thousand forty minutes""
