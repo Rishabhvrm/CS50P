9c4b09d1a913291ea7b005bdba4c4879aeea1d51
Rishabh Verma <rishabhvrm43@gmail.com>
2023-08-20 19:40:44 +0000

    Sun, Aug 20, 2023, 3:40 PM -04:00

--- 2023/Lecture-8-ObjectOrientedProgramming/seasons/seasons.py
+++ 2023/Lecture-8-ObjectOrientedProgramming/seasons/seasons.py
@@ -6,14 +6,15 @@ p = inflect.engine()
 def main():
     try:
         # expects date in YYYY-MM-DD format
-        date_obj = date.fromisoformat(input("Date of Birth: "))
+        d = input("Date of Birth: ")
     except:
         sys.exit("Invalid date")
     else:
         # print(calculate_minutes(date_obj).capitalize(), "minutes")
-        print(calculate_minutes(date_obj))
+        print(calculate_minutes(d))
 
-def calculate_minutes(d_obj):
+def calculate_minutes(d):
+    d_obj = date.fromisoformat(d)
     diff = date.today() - d_obj                         # calculate difference between today and given date
     minutes = (diff.days * 24 * 60)                     # convert days into minutes
 
